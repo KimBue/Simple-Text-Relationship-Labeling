2456545a8291b030e7f7db1fe1fbc84fdd4245e9
KimBue <kimbuergl@gmail.com>
2019-04-10 20:45:04 +0200

    labeling of just documents of a specific size, print metadata  for each file

--- TextLabeling.py
+++ TextLabeling.py
@@ -3,6 +3,8 @@ Main script
 author: Kim Bürgl
 """
 from view import *
+import math
+import nltk
 class LabelTexts:
     """
     Inputfile is the path to a File with one Document per Line
@@ -11,13 +13,40 @@ class LabelTexts:
     outputfilename is the name of the file created in this format per line: "X Y Z" where  X and why are indexes and Z is the label  given  by
     labels,  the path to  a file with each line another label, format as following: "k label" with k as a number and label as a string
     """
-    def __init__(self, inputfile, inputindexes, outputfilename, labels):
+    def __init__(self, inputfile, inputindexes, outputfilename, labels, nbOfPairsPerDoc = -1, docSize = math.inf, metaDataFile = None):
         self.inputfile = inputfile
         self.inputindexes = inputindexes
         self.outputfilename = outputfilename
         self.initLabels(labels)
         self.leftFileReader = FileIterator(self.inputfile)
         self.rightFileReader = FileIterator(self.inputfile)
+        self.nbOfPairsPerDoc = nbOfPairsPerDoc
+        self.lastLeftIndex = -1
+        self.docSize = docSize
+        if metaDataFile is not None:
+            self.left_metadata = FileIterator(metaDataFile)
+            self.right_metaData = FileIterator(metaDataFile)
+        else:
+            self.left_metadata = None
+            self.right_metaData = None
+
+
+
+    def limitSameDocument(self, index):
+        """
+        If there is a nbOfPairsPerDoc set, this function returns True if the limit is reached
+        :param index: current left index value
+        :return: this function returns True if the limit is reached
+        """
+        if index == self.lastLeftIndex:
+            self.countSameDocs += 1
+        else:
+            self.countSameDocs = 0
+        self.lastLeftIndex = index
+        if self.countSameDocs >= self.nbOfPairsPerDoc:
+            return True
+        else:
+            return False
 
     def initLabels(self, labels):
         self.labels = {}
@@ -35,12 +64,19 @@ class LabelTexts:
             if counter < startIndex:
                 continue #get to he start line
             index = l.split()
-            self.label(int(index[0]), (int(index[1])))
+            if self.nbOfPairsPerDoc != -1 and self.limitSameDocument(index[0]):
+                #reverse counter, because not presented candidates should not be part of the documnt limit
+                counter -= 1
+                continue
+            fileA, fileB = self.leftFileReader.getLine(int(index[0])), self.rightFileReader.getLine(int(index[1]))
+            if self.isTokenLengthLimit(fileA, fileB):
+                continue
+            self.label(fileA, fileB, int(index[0]), (int(index[1])))
             print("Candidate Number " + str(counter) +" labeled. Next one...")
 
-    def label(self, indexA, indexB):
-        textA = self.leftFileReader.getLine(indexA)
-        textB = self.rightFileReader.getLine(indexB)
+    def label(self, textA, textB, indexA, indexB):
+        if self.left_metadata is not None and self.right_metaData is not None:
+            print_metadata(self.left_metadata.getLine(indexA), self.right_metaData.getLine(indexB), 100)
         printTexts(textA, textB, 100)
         label = printLabelQuestion(self.labels)
         self.writeToFile(indexA, indexB, label)
@@ -49,6 +85,15 @@ class LabelTexts:
         f = open(self.outputfilename, 'a')
         f.write(str(indexA) + " " + str(indexB) + " " + str(label) + "\n")
         f.close()
+
+    def isTokenLengthLimit(self,textA, textB):
+        tokensA = nltk.word_tokenize(textA)
+        tokensB = nltk.word_tokenize(textB)
+        if len(tokensA)> self.docSize or len(tokensB) > self.docSize:
+            return True
+        else:
+            return False
+
 
 
 
@@ -83,9 +128,10 @@ class FileIterator:
 
 
 
+
 if __name__ == '__main__':
-    labelTexts = LabelTexts("examples/inputfile", "examples/candidate_indexes", "examples/out", "examples/labels")
-    labelTexts.start()
+    labelTexts = LabelTexts("examples/inputfile", "examples/candidate_indexes", "examples/out", "examples/labels", docSize=15000)
+    labelTexts.start(1)
 
 
 
